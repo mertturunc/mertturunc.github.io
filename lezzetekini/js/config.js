bd9bfada7871029b41510eac4d3c5e93d80461c4
Mert <mertturunc@gmail.com>
2023-03-06 00:50:11 +0300

    revert

--- lezzetekini/js/config.js
+++
@@ -1,8 +0,0 @@
-//mert's version
-export const MAPBOX_USERNAME_1 = 'mertturunc';
-export const MAPBOX_STYLE_ID_1 = 'clevexal800ho01o9hn9owlp4';
-export const MAPBOX_ACCESS_TOKEN_1 = 'pk.eyJ1IjoibWVydHR1cnVuYyIsImEiOiJjbGNzYTB6ZzgwMW55M29vZGIzdW96ZmMxIn0.-U-Cljwku_TEqYngV_e10w';
-//ekin's version
-export const MAPBOX_USERNAME_2 = 'galbrush';
-export const MAPBOX_STYLE_ID_2 = 'cle2ss7dn00az01kg6uvy7ab0';
-export const MAPBOX_ACCESS_TOKEN_2 = 'pk.eyJ1IjoiZ2FsYnJ1c2giLCJhIjoiY2xlMnF3cXc4MDFibTN0czQ3c3JwZTlzbSJ9.r_a4rOYpu-BDpOZFMj7zpQ';
