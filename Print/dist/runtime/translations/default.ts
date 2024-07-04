9edc14c7aff15631d1df5517d1f52e338384a4d6
Mert Turunç <mertturunc@gmail.com>
2024-07-04 13:41:54 +0300

    widget prod update

---
+++ Print/dist/runtime/translations/default.ts
@@ -0,0 +1,24 @@
+/**
+
+*/
+export default {
+  _widgetLabel: 'Print',
+  printTitle: 'Print Title',
+  layout: 'Layout',
+  format: 'Format',
+  print: 'Print',
+  advPrintOptions: 'Advanced Options',
+  author: 'Author',
+  copyright: 'Copyright',
+  printError: 'An error occurred while exporting the web map.',
+  printMessage:'Your printed files will appear here.',
+  scaleextent:'Map scale/extent',
+  preserve: 'Preserve',
+  mapscale: 'map scale',
+  mapextent: 'map extent',
+  layoutmeta: 'Layout metadata',
+  forcescale: 'Force scale',
+  current: 'current',
+  outSR: 'Output spatial reference WKID',
+  sbunit: 'Scale bar unit'
+}
