a2f565bbf2dced53aaff69732645cfbde32125a1
Mert Turunç <mertturunc@gmail.com>
2025-05-23 11:44:28 +0300

    Migrating this codebase to ubuntu 24 with the help of LLMs

--- measurement/src/runtime/translations/default.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  _widgetLabel: 'Measurement',
-  distMeasureTool: 'Distance Measurement Tool',
-  areaMearureTool: 'Area  Measurement Tool',
-  clearMeasure: 'Clear Measurements',
-  pointMeasureTool: 'Location Tool'
-}
